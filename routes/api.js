140b345055e611e0c42f66dc9b38f47d122bff76
Rohith Kumar Gattu <57759452+Rohithkumargattu979@users.noreply.github.com>
2021-06-13 11:40:12 +0530

    get request general structure

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,69 @@
 // Full Documentation - https://docs.turbo360.co
 const express = require('express')
-const router = express.Router()
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+const players = [
+  {
+    firstName:"eli", 
+    lastName:"manning", 
+    position:"qb", 
+    age:37, 
+    team:"nyg"
+  },
+  {
+    firstName:"tom", 
+    lastName:"brady", 
+    position:"qb", 
+    age: 41, 
+    team:"nep"
+  },
+  {
+    firstName:"jj", 
+    lastName:"thomson", 
+    postion:"de", 
+    age:28, 
+    team:"hou"
+  },
 
-/*  This is a sample API route. */
-
-router.get('/:resource', (req, res) => {
+]
+const teams = [
+  {
+    name:"giants", 
+    city:"new york", 
+    conference:"nfc"
+  },
+  {
+    name: "patriots", 
+    city:"new england", 
+    conference:"afc"
+  },
+  {
+    name:"texans",
+    city:"houston",
+    conference:"afc"
+  }
+]
+const db = {
+  team : teams,
+  player : players
+}
+router.get('/:resource', (req,res) =>{
+  const resource = req.params.resource
+  const data = db[resource]
+  if(data == null){
+    res.json({
+      confimation:"fail",
+      message:'Invalid Request'
+    })
+    return 
+  }
   res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    query: req.query // from the url query string
+    confimation:"success",
+    data: data
   })
 })
 
-router.get('/:resource/:id', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    id: req.params.id,
-    query: req.query // from the url query string
-  })
-})
+
 
 module.exports = router
